4413c8176e3cd34f2e6b599b0a43ccbf8f46bf59
surekhasrinivasan <surekhasrao@gmail.com>
2018-11-05 13:25:55 -0500

    Hackerrank Algorithm - Making Anagram - TDD

---
+++ MakingAnagrams/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MakingAnagrams
+{
+    public class MakeAnagram
+    {
+        public static int charactersDeleted(string str1, string str2)
+        {
+            if(str1 == str2)
+            {
+                return 0;
+            }
+            for(int i = 0; i < str1.Length; i++)
+            {
+                for(int j = 0; j < str2.Length; j++)
+                {
+                    if(str1[i] != str2[j])
+                    {
+                        return 2;
+                    }
+                }
+            }
+            return 0;
+        }
+    }
+}
---
+++ MakingAnagrams/MakingAnagramsTests.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MakingAnagrams
+{
+    [TestFixture]
+    class MakingAnagramsTests
+    {
+        [Test]
+        public void IsOutputAnInteger()
+        {
+            Assert.AreEqual(typeof(int), MakeAnagram.charactersDeleted("abc", "bcd").GetType());
+        }
+
+        [Test]
+        public void InputsSameReturnZero()
+        {
+            int result = MakeAnagram.charactersDeleted("abc", "abc");
+            Assert.AreEqual(0, result);
+        }
+
+        [Test]
+        public void CheckforTheOrderOfChars()
+        {
+            int result = MakeAnagram.charactersDeleted("abc", "cab");
+            Assert.AreEqual(0, result);
+        }
+
+        [Test]
+        public void InputOneDifferentCharReturns2()
+        {
+            int result = MakeAnagram.charactersDeleted("abd", "abc");
+            Assert.AreEqual(2, result);
+        }
+    }
+
+    
+}
